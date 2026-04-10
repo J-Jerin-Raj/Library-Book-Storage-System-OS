36d37ed60b9648be57d49a26850e434b0fb30d4c
JERIN RAJ <jerin@notavai.local>
2026-04-10 22:30:39 +0530

    Addition of Indexed Allocation methord.

--- OS_MicroProj.c
+++ OS_MicroProj.c
@@ -44,7 +44,7 @@ void link(){
         scanf("%d", &links[i]);
     }
     for (int i = 0; i < size; i++) {
-        if (memory[links[i]-1]==1){
+        if (memory[links[i]-1]==1 || size > memsize){
             printf("Allocation Failed (Blocks not free)\n");
             return;
        }
@@ -52,6 +52,59 @@ void link(){
     for (int i = 0; i < size; i++) {
         memory[links[i]-1] = 1;
     }
+    disp();
+}
+
+void indexA() {
+    int index, size;
+    int blocks[memsize];
+
+    printf("Enter index block: ");
+    scanf("%d", &index);
+
+    if (index < 1 || index > memsize) {
+        printf("Invalid index block\n");
+        return;
+    }
+
+    if (memory[index - 1] == 1) {
+        printf("Index block already allocated\n");
+        return;
+    }
+
+    printf("Enter number of blocks: ");
+    scanf("%d", &size);
+
+    if (size <= 0 || size > memsize) {
+        printf("Invalid size\n");
+        return;
+    }
+
+    printf("Enter block numbers: ");
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &blocks[i]);
+
+        // Validate each block
+        if (blocks[i] < 1 || blocks[i] > memsize) {
+            printf("Invalid block number\n");
+            return;
+        }
+
+        if (memory[blocks[i] - 1] == 1) {
+            printf("Allocation Failed (Block not free)\n");
+            return;
+        }
+    }
+
+    // Allocate index block
+    memory[index - 1] = 1;
+
+    // Allocate data blocks
+    for (int i = 0; i < size; i++) {
+        memory[blocks[i] - 1] = 1;
+    }
+
+    printf("Indexed Allocation Successful\n");
     disp();
 }
 
@@ -72,7 +125,7 @@ int main() {
                 link();
             }
             else if (choice == 3) {
-                // index();
+                indexA();
             }
             else if (choice == 4) {
                 disp();

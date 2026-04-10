2806111cfbd65f593c036cad13711c7f80c0c30f
JERIN RAJ <jerin@notavai.local>
2026-04-10 22:07:17 +0530

    Initial Commit with main function and declaration of Memory Block

---
+++ OS_MicroProj.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define memsize 50
+int memory[memsize] = {0};
+
+// Main function
+int main() {
+    int choice;
+    while (1) {
+        printf("\n--- Library Storage System ---\n");
+        printf("1) Contiguous Allocation\n2) Linked Allocation\n3) Indexed Allocation\n4) Display Memory\n5) Exit\n");
+
+        for (int i=0;i<1;i++) {
+            printf("Enter choice: ");
+            scanf("%d", &choice);
+            if (choice == 1) {
+                // conti();
+            }
+            else if (choice == 2) {
+                // link();
+            }
+            else if (choice == 3) {
+                // index();
+            }
+            else if (choice == 4) {
+                // disp();
+            }
+            else if (choice == 5) {
+                return 0;
+            }
+            else {
+                printf("Invalid Input\n");
+                i=-1;
+            }
+        }
+    }
+}
